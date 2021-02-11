3267b16c8ee0fe876c486e4c90838f3ff56d7b3d
Robby Ray <rgr5035@gmail.com>
2021-02-11 14:25:58 -0700

    added to service-worker and manifest

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,13 +1,18 @@
 const FILES_TO_CACHE = [
     "/",
     "/index.html",
-    ""
+    "/styles.css",
+    "index.js",
+    "manifest.webmanifest",
+    "/icons/icon-192x192.png",
+    "/icons/icon-512x512.png",
 ]
 
 const CACHE_NAME = "static-cache-v2";
-const DATA_CACHE = "";
+const DATA_CACHE = "data-cache-v1";
 
 console.log(self);
+console.log("service worker installed")
 
 self.addEventListener("install", function (e) {
     e.waitUntil(
@@ -21,16 +26,16 @@ self.addEventListener("install", function (e) {
     self.skipWaiting();
 })
 
-self.addEventListener("activate", function (e) {
-    e.waitUntil(
-        caches.keys().then((keylist) => {
-            return Promise.all(
+// self.addEventListener("activate", function (e) {
+//     e.waitUntil(
+//         caches.keys().then((keylist) => {
+//             return Promise.all(
 
-                keylist.map((key) => {
+//                 keylist.map((key) => {
 
-                    if (key !== CACHE_NAME && key !== DATA_CACHE_NAME)
-                })
-            )
-        })
-    )
-})
+//                     if (key !== CACHE_NAME && key !== DATA_CACHE_NAME)
+//                 })
+//             )
+//         })
+//     )
+// })
