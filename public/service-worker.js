d3fea8f445e221522eae9bff8a24c7a5fb6220d8
Robby Ray <rgr5035@gmail.com>
2021-02-11 16:42:39 -0700

    added to service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -9,10 +9,10 @@ const FILES_TO_CACHE = [
 ]
 
 const CACHE_NAME = "static-cache-v2";
-const DATA_CACHE = "data-cache-v1";
+const DATA_CACHE_NAME = "data-cache-v1";
 
 console.log(self);
-console.log("service worker installed")
+
 
 self.addEventListener("install", function (e) {
     e.waitUntil(
@@ -22,20 +22,63 @@ self.addEventListener("install", function (e) {
             return cache.addAll(FILES_TO_CACHE);
         })
     );
-
+    
+    //service worker skips the waiting phase during the install phase
     self.skipWaiting();
 })
 
-// self.addEventListener("activate", function (e) {
-//     e.waitUntil(
-//         caches.keys().then((keylist) => {
-//             return Promise.all(
+self.addEventListener("activate", function (e) {
+    e.waitUntil(
+        caches.keys().then((keylist) => {
+            return Promise.all(
 
-//                 keylist.map((key) => {
+                keylist.map((key) => {
 
-//                     if (key !== CACHE_NAME && key !== DATA_CACHE_NAME)
-//                 })
-//             )
-//         })
-//     )
-// })
+                    if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+                        console.log("Removing old cache data", key);
+                        return caches.delete(key);
+                    }
+                })
+            )
+        })
+    );
+
+    //claim method caused the pages to be controlled immediately
+    self.clients.claim();
+});
+
+self.addEventListener("fetch", function (e) {
+    if (e.request.url.includes("/api")) {
+        e.respondWith(
+
+            caches
+            .open(DATA_CACHE_NAME)
+            .then((cache) => {
+                return (
+                    fetch(e.request)
+                    .then((response) => {
+
+                        if (response.status === 200) {
+                            cache.put(e.request.url, response.clone());
+                        }
+
+                        return response;
+                    })
+                    .catch((err) => {
+                        return cache.match(e.request);
+                    })
+                );
+            })
+            .catch((err) => console.log(err))
+        );
+        return;
+    }
+    e.respondWith(
+        caches.open(CACHE_NAME).then((cache) => {
+
+            return cache.match(e.request).then((response) => {
+                return response || fetch(e.request);
+            });
+        })
+    );
+});
