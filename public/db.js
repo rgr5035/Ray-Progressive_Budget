685d0ee36e603d342149588d7846dc5d6c0fb58a
Robby Ray <rgr5035@gmail.com>
2021-02-12 17:32:57 -0700

    updating db.js

--- public/db.js
+++ public/db.js
@@ -35,40 +35,36 @@ function saveRecord(record) {
 }
 
 function checkDatabase() {
-
-    //creates a transaction on the db with read/write access
+    // open a transaction on your pending db
     const transaction = db.transaction(["pending"], "readwrite");
-
-    //access the pending object store
+    // access your pending object store
     const store = transaction.objectStore("pending");
-
-    //get all records from the store and set it to a variable
+    // get all records from store and set to a variable
     const getAll = store.getAll();
-
-    getAll.onsuccess = function() {
-        if (getAll.result.length > 0) {
-            fetch("/api/transaction/bulk", {
-                method: "POST",
-                body: JSON.stringify(getAll.result),
-                headers: {
-                    Accept: "application/json, text/plain, */*",
-                    "Content-Type": "application/json"
-                }
-            })
-            .then(response => response.json())
-            .then(() => {
-
-                //creates a transaction on the db with read/write access
-                const transaction = db.transaction(["pending"], "readwrite");
-
-                //access the pending object store
-                const store = transaction.objectStore("pending");
-                
-                // clear all items in the store
-                store.clear();
-            })
-        }
+  
+    getAll.onsuccess = function () {
+      if (getAll.result.length > 0) {
+        fetch("/api/transaction/bulk", {
+          method: "POST",
+          body: JSON.stringify(getAll.result),
+          headers: {
+            Accept: "application/json, text/plain, */*",
+            "Content-Type": "application/json",
+          },
+        })
+          .then((response) => response.json())
+          .then(() => {
+            // if successful, open a transaction on your pending db
+            const transaction = db.transaction(["pending"], "readwrite");
+  
+            // access your pending object store
+            const store = transaction.objectStore("pending");
+  
+            // clear all items in your store
+            store.clear();
+          });
+      }
     };
-}
+  }
 
 window.addEventListener("online", checkDatabase);
